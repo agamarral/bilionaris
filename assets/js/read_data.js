dc5316dc8eab645b369f307fd0afb077654a8b87
agamarral@uoc.edu <agamarral@uoc.edu>
2023-12-27 12:00:34 +0100

    Code reorganization

--- assets/js/read_data.js
+++
@@ -1,3 +0,0 @@
-import bpc from '../data/billionaires_per_country.json' assert { type: 'json' };
-import bpb from '../data/billionaires_per_business.json' assert { type: 'json' };
-import bpa from '../data/billionaires_per_age.json' assert { type: 'json' };
